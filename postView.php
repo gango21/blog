8a992519d6c710d740f4960216990b93c7facef1
Morgan Taglang <morgan.tag@gmail.com>
2018-10-30 09:47:47 +0100

    postView.php

---
+++ postView.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon blog</title>
+        <link href="style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        <h1>Mon super blog !</h1>
+        <p><a href="index.php">Retour à la liste des billets</a></p>
+
+        <div class="news">
+            <h3>
+                <?= htmlspecialchars_decode($post->title()) ?>
+                <em>le <?php echo $post->date_creation() ?></em>
+            </h3>
+
+            <p>
+                <?= nl2br(htmlspecialchars_decode($post->content())) ?>
+            </p>
+        </div>
+
+        <h2>Commentaires</h2>
+
+
+        <?php
+    for ($i=0; $i<count($comments); $i++){?>
+        <p><strong><?php echo htmlspecialchars_decode($comments[$i]->author()) ?></strong> le <?php echo $comments[$i]->date_comment() ?></p>
+        <p><?php echo nl2br(htmlspecialchars_decode($comments[$i]->content_comment())) ?></p>
+        <?php
+        }
+        ?>
+    </body>
+</html>
