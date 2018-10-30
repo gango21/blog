857bf1077df480935f53775c86c8dc1825ea60fa
Morgan Taglang <morgan.tag@gmail.com>
2018-10-30 09:46:56 +0100

    class Commentmanager

---
+++ Commentmanager.php
@@ -0,0 +1,35 @@
+<?php
+
+require ("Comment.php");
+
+class CommentManager
+{
+    //Connexion à la base de données
+
+    private $_db;
+
+    public function __construct($db)
+    {
+        $this->setDb($db);
+    }
+
+    public function getComments($id)
+    {
+        $q = $this->_db->query('SELECT id, id_post, author, content_comment, date_comment FROM comments WHERE id_post = '.$id);
+
+        $comments = [];
+
+        while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+        {
+            $comments[] = new Comment($donnees);
+        }
+        return $comments;
+    }
+
+    public function setDb(PDO $db)
+    {
+        $this->_db = $db;
+    }
+}
+
+?>
