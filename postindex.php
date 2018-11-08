97cd238d0ded818dd3da19f84fe00059992a77c9
Morgan Taglang <morgan.tag@gmail.com>
2018-11-08 18:53:41 +0100

    date server

--- postindex.php
+++ postindex.php
@@ -7,13 +7,18 @@ $postManager = new PostManager($db);
 $post = $postManager->getPost($_GET['postId']);
 $commentManager = new CommentManager($db);
 
-if (isset($_POST['id_post']) && isset($_POST['author']) && isset($_POST['content_comment']) && isset($_POST['date_comment']))
+if (isset($_POST['id_post']) && isset($_POST['author']) && isset($_POST['content_comment']))
 {
     $postId = $_POST['id_post'];
     $author = $_POST['author'];
     $content_comment = $_POST['content_comment'];
-    $date_comment = $_POST['date_comment'];
-    $commentManager->addComment($postId, $author, $content_comment, $date_comment);
+    $commentManager->addComment($postId, $author, $content_comment);
+}
+
+if (isset($_POST['signal_comment']))
+{
+    $id = $_POST['id'];
+    $commentManager->signalComment($id);
 }
 
 $comments = $commentManager->getComments($_GET['postId']);
