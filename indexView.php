0314d3e7b1bbb6655223538b974e9b93b3faab70
Morgan Taglang <morgan.tag@gmail.com>
2018-11-08 18:52:58 +0100

    template

--- indexView.php
+++ indexView.php
@@ -1,12 +1,7 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mon blog</title>
-        <link href="style.css" rel="stylesheet" />
-    </head>
-
-    <body>
+    <?php $title="Blog"; ?>
+    <?php ob_start(); ?>
         <h1>Blog</h1>
         <p>Derniers billets du blog :</p>
         <div class="news">
@@ -19,5 +14,9 @@
                 }
                 ?>
         </div>
-    </body>
+    <footer>
+        <a href="admin.php">Admin</a>
+    </footer>
+    <?php $content = ob_get_clean(); ?>
+    <?php require('template.php'); ?>
 </html>
