eadbe3e9b598106220c4a4f1844328c4b9c7818a
Morgan Taglang <morgan.tag@gmail.com>
2018-11-09 11:45:22 +0100

    addpostView

---
+++ addpostView.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <?php $title="Ajouter un post"; ?>
+    <?php ob_start(); ?>
+    <h1>Blog</h1>
+    <p>Ajouter un post :</p>
+
+    <script src='https://devpreview.tiny.cloud/demo/tinymce.min.js'></script>
+    <script>
+        tinymce.init({
+            selector: '#mytextarea'
+        });
+    </script>
+
+    <body>
+        <form method="post">
+            <input type="text" name="titlepost" value="Titre du Post"><br>
+            <textarea id="mytextarea" name="content">Hello, World!</textarea>
+            <input type="submit"  value="Envoyer">
+        </form>
+    </body>
+
+    <?php $content = ob_get_clean(); ?>
+    <?php require('template.php'); ?>
+</html>
