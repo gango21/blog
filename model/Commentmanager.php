a96d8cd9f1614c74ae7fcabf910787b24939cd60
Morgan Taglang <morgan.tag@gmail.com>
2018-11-19 11:46:37 +0100

    vers dossier model

---
+++ model/Commentmanager.php
@@ -0,0 +1,71 @@
+<?php
+
+require ("Comment.php");
+
+class CommentManager
+{
+    //Connexion à la base de données
+
+    private $_db;
+
+    public function __construct($db)
+    {
+        $this->setDb($db);
+    }
+
+    public function getComments($id)
+    {
+        $q = $this->_db->query('SELECT id, id_post, author, content_comment, date_comment, signal_comment FROM comments WHERE id_post = '.$id);
+
+        $comments = [];
+
+        while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+        {
+            $comments[] = new Comment($donnees);
+        }
+        return $comments;
+    }
+
+    public function addComment($postId, $author, $content_comment)
+    {
+        $signal_comment = 0;
+        $date_comment = date("Y-m-d H:i:s");
+        $q = $this->_db->prepare('INSERT INTO comments(id_post, author, content_comment, date_comment, signal_comment) VALUES(?, ?, ?, ?, ?)');
+        $q->execute(array($postId, $author, $content_comment, $date_comment, $signal_comment));
+    }
+
+    public function signalComment($id)
+    {
+        $q = $this->_db->exec('UPDATE comments SET signal_comment=1 WHERE id=' .$id);
+    }
+
+    public function ignoreSignaledComment($id)
+    {
+        $q = $this->_db->exec('UPDATE comments SET signal_comment=0 WHERE id=' .$id);
+    }
+
+    public function getSignaledComments()
+    {
+        $q = $this->_db->query('SELECT id, id_post, author, content_comment, date_comment, signal_comment FROM comments WHERE signal_comment = 1');
+
+        $signaledcomments = [];
+
+        while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+        {
+            $signaledcomments[] = new Comment($donnees);
+        }
+        return $signaledcomments;
+    }
+
+    public function deleteComment($id)
+    {
+        $q = $this->_db->exec('DELETE FROM comments WHERE id=' .$id);
+    }
+
+    public function setDb(PDO $db)
+    {
+        $this->_db = $db;
+    }
+}
+
+?>
---
+++ model/Postmanager.php
@@ -0,0 +1,71 @@
+<?php
+require 'Post.php';
+
+class PostManager
+
+{
+    //Connexion à la base de données
+
+    private $_db;
+
+    public function __construct($db)
+    {
+        $this->setDb($db);
+    }
+
+    //Récupérer données de la table posts
+    public function getPosts()
+    {
+
+        $posts=[];
+
+        $q = $this->_db->query('SELECT id, title, content, date_creation FROM posts');
+
+        while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+        {
+            $posts[] = new Post($donnees);
+        }
+        return $posts;
+    }
+
+
+    //Récupèrer un post en particulier
+    public function getPost($id)
+    {
+        $id = (int) $id;
+
+        $q = $this->_db->query('SELECT id, title, content, date_creation FROM posts WHERE id = '.$id);
+        $donnees = $q->fetch(PDO::FETCH_ASSOC);
+
+        return new Post($donnees);
+    }
+
+    // Ajouter un post
+    public function addPost($title, $content)
+    {
+        $date = date("Y-m-d H:i:s");
+        $q = $this->_db->prepare('INSERT INTO posts(title, content, date_creation) VALUES(?, ?, ?)');
+        $q->execute(array($title, $content, $date));
+    }
+
+    // Editer un post
+    public function editPost($id, $title, $content)
+    {
+        $date = date("Y-m-d H:i:s");
+        $querry = 'UPDATE posts SET title ="' .$title.'", content = "'.$content.'", date_creation = "'.$date.'" WHERE id=' .$id;
+        $q = $this->_db->exec($querry);
+    }
+
+    //Supprimer un post
+    public function deletePost($id)
+    {
+        $q = $this->_db->exec('DELETE FROM posts WHERE id=' .$id);
+    }
+
+    public function setDb(PDO $db)
+    {
+        $this->_db = $db;
+    }
+
+}
+?>
