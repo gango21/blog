a9fd45d9aa1249dd89d5dda3154e144f8ca56119
Morgan Taglang <morgan.tag@gmail.com>
2018-11-19 11:47:20 +0100

    modifs url

--- template.php
+++ template.php
@@ -6,8 +6,8 @@
         <link href="style.css" rel="stylesheet" />
     </head>
     <header>
-        <a href="index.php">Liste des billets</a>
-        <a href="admin.php">Admin</a>
+        <a href="index.php?action=listPosts">Liste des billets</a>
+        <a href="index.php?action=admin">Admin</a>
     </header>
     <body>
         <?= $content ?>
