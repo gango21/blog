97367e874f3aacf6b0fbeb911f9f89dc201c3640
Morgan Taglang <morgan.tag@gmail.com>
2018-11-19 11:46:22 +0100

    index devient routeur

--- index.php
+++ index.php
@@ -1,9 +1,46 @@
 <?php
-require('Postmanager.php');
+require('controller/controller.php');
 
-$db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
-$postManager = new PostManager($db);
-$posts = $postManager->getPosts();
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == 'listPosts') {
+        listPosts();
+    }
+    elseif ($_GET['action'] == 'post') {
+        if (isset($_GET['postId']) && $_GET['postId'] > 0) {
+            post();
+        }
+        else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+        }
+    }
+    elseif ($_GET['action'] == 'admin') {
+        admin();
+    }
+    //admin
+    elseif ($_GET['action'] == 'addPost') {
+            addPost();
+        }
+    elseif ($_GET['action'] == 'editPost') {
+        echo $_SESSION['admin'];
+        editPost();
+    }
+    elseif ($_GET['action'] == 'editPostForm') {
+        if (isset($_GET['id']) && $_GET['id'] > 0){
+            editPostForm();
+        }
+        else{
+            echo 'Erreur : aucun identifiant de billet envoyé';
+        }
+    }
+    elseif ($_GET['action'] == 'deleteComment') {
+        deleteComment();
+    }
+    elseif ($_GET['action'] == 'signaledComments') {
+        signaledComments();
+    }
+}
+else {
+    listPosts();
+}
 
-require('indexView.php');
 ?>
