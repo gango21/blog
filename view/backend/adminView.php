924548148971da4b18f1aae421151b93301fc7a7
Morgan Taglang <morgan.tag@gmail.com>
2018-11-23 10:15:06 +0100

    editer mot de passe lien

--- view/backend/adminView.php
+++ view/backend/adminView.php
@@ -4,8 +4,9 @@
     <?php ob_start();
 
     // Le mot de passe n'a pas été envoyé ou n'est pas bon
-    if (!isset($_POST['user']) OR !isset($_POST['password']) OR ($_POST['user'] != "admin" OR $_POST['password'] != "password"))
+    if (!isset($_SESSION['admin']))
     {
+
     ?>
     <h1>Admin</h1>
         <p>Entrez votre nom d'utilisateur et votre mot de passe</p>
@@ -24,9 +25,9 @@
         <p><a href="index.php?action=addPost">Ajouter un billet</a></p>
         <p><a href="index.php?action=editPost">Modifier un billet</a></p>
         <p><a href="index.php?action=deleteComment">Modérer les commentaires</a></p>
+        <p><a href="index.php?action=editPassword">Modifier le mot de passe</a></p>
     <?php
     }
-
     $content = ob_get_clean();
     require('template.php');
     ?>
