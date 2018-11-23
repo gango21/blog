206302599f7285a164de44967dc09dfa1f4e3a07
Morgan Taglang <morgan.tag@gmail.com>
2018-11-23 11:16:01 +0100

    interdire l'accès aux fonctions d'admin si pas de session

--- controller/controller.php
+++ controller/controller.php
@@ -84,8 +84,7 @@ function addPost()
 
     if(empty($_SESSION['admin']))
     {
-        // Si inexistante ou nulle, on redirige vers le formulaire de login
-        header('Location: ');
+        header('Location: index.php');
         exit();
     }
 
@@ -108,92 +107,132 @@ function addPost()
 
 function deleteComment()
 {
-    $db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
-    $commentManager = new CommentManager($db);
-
-    if (isset($_POST['id_delete']))
+    if(empty($_SESSION['admin']))
     {
-        $id = $_POST['id_delete'];
-        $commentManager->deleteComment($id);
+        header('Location: index.php');
+        exit();
     }
 
-    if (isset($_POST['id_ignore']))
+    else
     {
-        $id = $_POST['id_ignore'];
-        $commentManager->ignoreSignaledComment($id);
+        $db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+        $commentManager = new CommentManager($db);
+
+        if (isset($_POST['id_delete']))
+        {
+            $id = $_POST['id_delete'];
+            $commentManager->deleteComment($id);
+        }
+
+        if (isset($_POST['id_ignore']))
+        {
+            $id = $_POST['id_ignore'];
+            $commentManager->ignoreSignaledComment($id);
+        }
+
+        $signaledcomments = $commentManager->getSignaledComments();
+
+
+        require('view/backend/deletecommentView.php');
     }
 
-    $signaledcomments = $commentManager->getSignaledComments();
-
-
-    require('view/backend/deletecommentView.php');
 }
 
 
 
 function editPost()
 {
-    $db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
-    $postmanager = new PostManager($db);
-    $posts = $postmanager->getPosts();
-
-    if (isset($_POST['post_delete']))
+    if(empty($_SESSION['admin']))
     {
-        $id = $_POST['post_delete'];
-        $postmanager->deletePost($id);
+        header('Location: index.php');
+        exit();
     }
 
-    require('view/backend/editpostView.php');
+    else
+    {
+        $db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+        $postmanager = new PostManager($db);
+        $posts = $postmanager->getPosts();
+
+        if (isset($_POST['post_delete']))
+        {
+            $id = $_POST['post_delete'];
+            $postmanager->deletePost($id);
+        }
+
+        require('view/backend/editpostView.php');
+    }
+
 }
 
 function editPostForm()
 {
-    $db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
-    $postmanager = new PostManager($db);
-
-    if (isset($_GET['id']))
+    if(empty($_SESSION['admin']))
     {
-        $id = $_GET['id'];
-        $post = $postmanager->getPost($_GET['id']);
+        header('Location: index.php');
+        exit();
+    }
+    else
+    {
+        $db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+        $postmanager = new PostManager($db);
+
+        if (isset($_GET['id']))
+        {
+            $id = $_GET['id'];
+            $post = $postmanager->getPost($_GET['id']);
+        }
+
+        if (isset($_POST['titlePostEdit']) && isset($_POST['idPostEdit']) && isset($_POST['contentPostEdit']))
+        {
+            $id = $_POST['idPostEdit'];
+            $titlepost = $_POST['titlePostEdit'];
+            $content = $_POST['contentPostEdit'];
+            $postmanager->editPost($id, $titlepost, $content);
+        }
+
+        require('view/backend/editpostformView.php');
     }
 
-    if (isset($_POST['titlePostEdit']) && isset($_POST['idPostEdit']) && isset($_POST['contentPostEdit']))
-    {
-        $id = $_POST['idPostEdit'];
-        $titlepost = $_POST['titlePostEdit'];
-        $content = $_POST['contentPostEdit'];
-        $postmanager->editPost($id, $titlepost, $content);
-    }
-
-    require('view/backend/editpostformView.php');
 }
 
 function signaledComments()
 {
-    require('Commentmanager.php');
 
-    $db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
-    $postManager = new PostManager($db);
-    $post = $postManager->getPost($_GET['postId']);
-    $commentManager = new CommentManager($db);
-
-    if (isset($_POST['id_post']) && isset($_POST['author']) && isset($_POST['content_comment']))
+    if(empty($_SESSION['admin']))
     {
-        $postId = $_POST['id_post'];
-        $author = $_POST['author'];
-        $content_comment = $_POST['content_comment'];
-        $commentManager->addComment($postId, $author, $content_comment);
+        header('Location: index.php');
+        exit();
     }
 
-    if (isset($_POST['signal_comment']))
+    else
     {
-        $id = $_POST['id'];
-        $commentManager->signalComment($id);
+        require('Commentmanager.php');
+
+        $db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+        $postManager = new PostManager($db);
+        $post = $postManager->getPost($_GET['postId']);
+        $commentManager = new CommentManager($db);
+
+        if (isset($_POST['id_post']) && isset($_POST['author']) && isset($_POST['content_comment']))
+        {
+            $postId = $_POST['id_post'];
+            $author = $_POST['author'];
+            $content_comment = $_POST['content_comment'];
+            $commentManager->addComment($postId, $author, $content_comment);
+        }
+
+        if (isset($_POST['signal_comment']))
+        {
+            $id = $_POST['id'];
+            $commentManager->signalComment($id);
+        }
+
+        $comments = $commentManager->getComments($_GET['postId']);
+
+        require('view/frontend/postView.php');
     }
 
-    $comments = $commentManager->getComments($_GET['postId']);
-
-    require('view/frontend/postView.php');
 }
 
 
