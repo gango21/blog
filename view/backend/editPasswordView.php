86c2c30257fe0ba6ab4158703068375fb39163f7
Morgan Taglang <morgan.tag@gmail.com>
2018-11-23 10:15:20 +0100

    pade d'edition du mot de passe

---
+++ view/backend/editPasswordView.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <?php $title="Changer de mot de passe"; ?>
+    <?php ob_start(); ?>
+    <h1>Blog</h1>
+
+    <body>
+        <form method="post">
+            <input type="text" name="id" value="<?php echo $admin['id'] ?>" hidden><br>
+            Nouveau mot de passe
+            <input type="password" name="password" value=""><br>
+            <input type="submit"  value="Envoyer">
+        </form>
+    </body>
+
+    <?php $content = ob_get_clean(); ?>
+    <?php require('template.php'); ?>
+</html>
