01eca2571f4e896b56be3b5149c4ee7089eef860
Morgan Taglang <morgan.tag@gmail.com>
2018-11-30 11:09:08 +0100

    affichage loged in / loged out

--- template.php
+++ template.php
@@ -4,23 +4,27 @@
         <meta charset="utf-8" />
         <title><?= $title ?></title>
         <link href="style.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     </head>
     <header>
-        <a href="index.php?action=listPosts">Liste des billets</a>
-        <?php
-            if (!isset($_SESSION['admin'])){
-        ?>
-        <a href="index.php?action=admin">Admin</a>
+        <ul>
+            <li>
+                <a href="index.php?action=listPosts">Liste des billets</a>
+            </li>
+            <li>
+                <a href="index.php?action=admin">Admin</a>
+            </li>
             <?php
-        }
-            ?>
-        <?php
             if (isset($_SESSION['admin'])){
-                ?>
-        <a href="index.php?action=logout">Déconnexion</a>
-        <?php
-            }
-        ?>
+            ?>
+            <li>
+                <a href="index.php?action=logout">Déconnexion</a>
+            </li>
+            <?php
+                }
+            ?>
+        </ul>
+
     </header>
     <body>
         <?= $content ?>
