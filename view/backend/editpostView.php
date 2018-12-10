fe550767bfa31df254a7c47c4ebdf31b54ebb408
Morgan Taglang <morgan.tag@gmail.com>
2018-12-10 11:30:28 +0100

    div for css

--- view/backend/editpostView.php
+++ view/backend/editpostView.php
@@ -3,24 +3,45 @@
     <?php $title="Editer un post"; ?>
     <?php ob_start(); ?>
     <h1>Blog</h1>
-    <p>Ajouter un post :</p>
-    <div class="news">
+
         <?php
         for ($i=0; $i<count($posts); $i++){?>
-        <h3><?php echo $posts[$i]->title();?> </h3>
-        <p><?php echo $posts[$i]->content();?></p>
-        <form method="post" action="index.php?action=editPostForm&id=<?php echo $posts[$i]->id(); ?>">
-            <input type="hidden" name="id" value="<?php echo $posts[$i]->id(); ?>">
-            <input type="submit" value="Modifier">
-        </form>
+            <!--<div class="post_container">
+                <div class="header">
+                    <h3><?php echo $posts[$i]->title();?> </h3>
+                </div>
+                    <p><?php echo $posts[$i]->content();?></p>
+                    <form method="post" action="index.php?action=editPostForm&id=<?php echo $posts[$i]->id(); ?>">
+                        <input type="hidden" name="id" value="<?php echo $posts[$i]->id(); ?>">
+                        <input type="submit" value="Modifier">
+                    </form>
+                    <form method="post">
+                        <input type="hidden" name="post_delete" value="<?php echo $posts[$i]->id(); ?>">
+                        <input type="submit" value="Supprimer">
+                    </form>
+    </div>
+                    <?php
+                                                      }
+                    ?>
+    -->
+    <?php
+    for ($i=0; $i<count($posts); $i++){?>
+    <div class="post_container">
+        <div class="header">
+            <h3><i class="far fa-file-alt"></i><?php echo $posts[$i]->title();?></h3><h3><i class="far fa-calendar-alt"></i><?php echo $posts[$i]->date_creation();?><?php echo $posts[$i]->date_creation_fr();?></h3>
+        </div>
+        <div class="content">
+            <p><?php echo $posts[$i]->content();?></p>
+        </div>
         <form method="post">
             <input type="hidden" name="post_delete" value="<?php echo $posts[$i]->id(); ?>">
             <input type="submit" value="Supprimer">
         </form>
-        <?php
-                                          }
-        ?>
     </div>
+    <?php
+                                      }
+
+    ?>
     <?php $content = ob_get_clean(); ?>
     <?php require('template.php'); ?>
 </html>
