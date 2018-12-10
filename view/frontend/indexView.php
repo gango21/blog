c4e372fa283453a6c85857333cb7c1ce8ddd3320
Morgan Taglang <morgan.tag@gmail.com>
2018-12-10 11:30:39 +0100

    div for css

--- view/frontend/indexView.php
+++ view/frontend/indexView.php
@@ -7,19 +7,25 @@
                 for ($i=0; $i<count($posts); $i++){?>
                    <div class="post_container">
                       <div class="header">
-                          <h3><i class="far fa-file-alt"></i><?php echo $posts[$i]->title();?></h3><h3><i class="far fa-calendar-alt"></i><?php echo $posts[$i]->date_creation();?></h3>
+                          <div class="title"><i class="far fa-file-alt"></i><?php echo $posts[$i]->title();?></div>
+                          <div class="date_creation"><i class="far fa-calendar-alt"></i><?php echo date("d-m-Y H:i",strtotime($posts[$i]->date_creation()))?><?php ?></div>
                       </div>
                        <div class="content">
                     <p><?php echo $posts[$i]->content();?></p>
                        </div>
-                    <a href="index.php?action=post&postId=<?php echo $posts[$i]->id(); ?>">Commentaires</a>
-            </div>
+                       <div class="comment">
+                       <a href="index.php?action=post&postId=<?php echo $posts[$i]->id(); ?>"><i class="far fa-comment"></i>Commentaires</a>
+                       </div>
+                    </div>
                 <?php
                 }
 
                 ?>
-            <a href="index.php?action=listPosts&page=<?php echo $page-1 ?>">Précédent</a>
-            <a href="index.php?action=listPosts&page=<?php echo $page+1 ?>">Suivant</a>
+                <div class="page_navigation">
+                    <a href="index.php?action=listPosts&page=<?php echo $page-1 ?>"><i class="fas fa-caret-left"></i></a>
+                    <?php echo $page ?>
+                    <a href="index.php?action=listPosts&page=<?php echo $page+1 ?>"><i class="fas fa-caret-right"></i></a>
+            </div>
         </div>
     <?php $content = ob_get_clean(); ?>
     <?php require('template.php'); ?>
