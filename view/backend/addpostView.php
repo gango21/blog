7b6adc3ea26bf58509ad6014cf4e3535ee80274c
Morgan Taglang <morgan.tag@gmail.com>
2018-12-14 11:34:08 +0100

    balises pour style

--- view/backend/addpostView.php
+++ view/backend/addpostView.php
@@ -2,8 +2,6 @@
 <html>
     <?php $title="Ajouter un post"; ?>
     <?php ob_start(); ?>
-    <h1>Blog</h1>
-    <p>Ajouter un post :</p>
 
     <script src='https://devpreview.tiny.cloud/demo/tinymce.min.js'></script>
     <script>
@@ -13,11 +11,15 @@
     </script>
 
     <body>
-        <form method="post" action="">
-            <input type="text" name="titlepost" value="Titre du Post"><br>
-            <textarea id="mytextarea" name="content">Hello, World!</textarea>
-            <input type="submit"  value="Envoyer">
-        </form>
+        <div class="edit_page">
+            <div class="edit">
+                <form method="post" action="">
+                    <input type="text" name="titlepost" value="Titre du Post"><br>
+                    <textarea id="mytextarea" name="content">Hello, World!</textarea>
+                    <input type="submit"  value="Envoyer">
+                </form>
+            </div>
+        </div>
     </body>
 
     <?php $content = ob_get_clean(); ?>
