8d62e83375660565b9a0af8f7a60d1bb70468123
Morgan Taglang <morgan.tag@gmail.com>
2018-12-14 11:35:26 +0100

    style edition d'un post

--- view/backend/editpostformView.php
+++ view/backend/editpostformView.php
@@ -2,8 +2,6 @@
 <html>
     <?php $title="Ajouter un post"; ?>
     <?php ob_start(); ?>
-    <h1>Blog</h1>
-    <p>Ajouter un post :</p>
 
     <script src='https://devpreview.tiny.cloud/demo/tinymce.min.js'></script>
     <script>
@@ -13,12 +11,16 @@
     </script>
 
     <body>
-        <form method="post">
-            <input type="text" name="idPostEdit" value="<?php echo $post->id() ?>" hidden><br>
-            <input type="text" name="titlePostEdit" value="<?php echo $post->title() ?>"><br>
-            <textarea id="mytextarea" name="contentPostEdit"><?php echo $post->content() ?></textarea>
-            <input type="submit"  value="Envoyer">
-        </form>
+        <div class="edit_page">
+            <div class="edit">
+                <form method="post">
+                    <input type="text" name="idPostEdit" value="<?php echo $post->id() ?>" hidden>
+                    <input type="text" name="titlePostEdit" value="<?php echo $post->title() ?>"><br>
+                    <textarea id="mytextarea" name="contentPostEdit"><?php echo $post->content() ?></textarea>
+                    <input type="submit"  value="Envoyer">
+                </form>
+            </div>
+        </div>
     </body>
 
     <?php $content = ob_get_clean(); ?>
