c5efefaacecb42a8d4a2ad0249b6f147c5ba3d95
Morgan Taglang <morgan.tag@gmail.com>
2019-01-04 17:26:41 +0100

    indentation

--- view/backend/editPasswordView.php
+++ view/backend/editPasswordView.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html>
-    <?php $title="Changer de mot de passe"; ?>
-    <?php ob_start(); ?>
+<?php $title="Changer de mot de passe"; ?>
+<?php ob_start(); ?>
 
-    <body>
-        <form method="post">
-            <input type="text" name="id" value="<?php echo $admin['id'] ?>" hidden><br>
-            Nouveau mot de passe
-            <input type="password" name="password" value=""><br>
-            <input type="submit"  value="Envoyer">
-        </form>
-    </body>
+<body>
+    <form method="post">
+        <input type="text" name="id" value="<?php echo $admin['id'] ?>" hidden><br>
+        Nouveau mot de passe
+        <input type="password" name="password" value=""><br>
+        <input type="submit" value="Envoyer">
+    </form>
+</body>
 
-    <?php $content = ob_get_clean(); ?>
-    <?php require('template.php'); ?>
+<?php $content = ob_get_clean(); ?>
+<?php require('view/template.php'); ?>
+
 </html>
