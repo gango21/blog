9189e07804ff51f431ec280c8f6e1fd8c46698a9
Morgan Taglang <morgan.tag@gmail.com>
2019-01-04 17:27:29 +0100

    lien vers template.php modifié

--- view/frontend/indexView.php
+++ view/frontend/indexView.php
@@ -1,32 +1,39 @@
 <!DOCTYPE html>
 <html>
-    <?php $title="Blog"; ?>
-    <?php ob_start(); ?>
-        <div class="news">
-                <?php
+<?php $title="Blog"; ?>
+<?php ob_start(); ?>
+<div class="news">
+    <?php
                 for ($i=0; $i<count($posts); $i++){?>
-                   <div class="post_container">
-                      <div class="header">
-                          <div class="title"><span><i class="far fa-file-alt"></i><?php echo $posts[$i]->title();?></span></div>
-                          <div class="date_creation"><span><i class="far fa-calendar-alt"></i><?php echo date("d-m-Y H:i",strtotime($posts[$i]->date_creation()))?><?php ?></span></div>
-                      </div>
-                       <div class="content">
-                    <p><?php echo $posts[$i]->content();?></p>
-                       </div>
-                       <div class="comment">
-                           <a href="index.php?action=post&postId=<?php echo $posts[$i]->id(); ?>"><i class="far fa-comment"></i><?php echo $commentManager->countComments($posts[$i]->id()); ?> commentaire(s)</a>
-                       </div>
-                    </div>
-                <?php
+    <div class="post_container">
+        <div class="header">
+            <div class="title"><span><i class="far fa-file-alt"></i>
+                    <?php echo $posts[$i]->title();?></span></div>
+            <div class="date_creation"><span><i class="far fa-calendar-alt"></i>
+                    <?php echo date("d-m-Y H:i",strtotime($posts[$i]->date_creation()))?>
+                    <?php ?></span></div>
+        </div>
+        <div class="content">
+            <p>
+                <?php echo $posts[$i]->content();?>
+            </p>
+        </div>
+        <div class="comment">
+            <a href="index.php?action=post&postId=<?php echo $posts[$i]->id(); ?>"><i class="far fa-comment"></i>
+                <?php echo $commentManager->countComments($posts[$i]->id()); ?> commentaire(s)</a>
+        </div>
+    </div>
+    <?php
                 }
 
                 ?>
-                <div class="page_navigation">
-                    <a href="index.php?action=listPosts&page=<?php echo $page-1 ?>"><i class="fas fa-caret-left"></i></a>
-                    <?php echo $page ?>
-                    <a href="index.php?action=listPosts&page=<?php echo $page+1 ?>"><i class="fas fa-caret-right"></i></a>
-            </div>
-        </div>
-    <?php $content = ob_get_clean(); ?>
-    <?php require('template.php'); ?>
+    <div class="page_navigation">
+        <a href="index.php?action=listPosts&page=<?php echo $page-1 ?>"><i class="fas fa-caret-left"></i></a>
+        <?php echo $page ?>
+        <a href="index.php?action=listPosts&page=<?php echo $page+1 ?>"><i class="fas fa-caret-right"></i></a>
+    </div>
+</div>
+<?php $content = ob_get_clean(); ?>
+<?php require('view/template.php'); ?>
+
 </html>
