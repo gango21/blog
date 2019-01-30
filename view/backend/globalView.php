71919d55d0fcff5c2b303f8adfce10bfacaba48c
Morgan Taglang <morgan.tag@gmail.com>
2019-01-30 15:07:11 +0100

    vue d'ensemble améliorée

--- view/backend/globalView.php
+++ view/backend/globalView.php
@@ -5,12 +5,12 @@
 
 <div class="global_page">
     <div class="global">
-        <style type="text/css">
+        <!--<style type="text/css">
             .tg  {border-collapse:collapse;border-spacing:0;}
             .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
             .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
             .tg .tg-0lax{text-align:left;vertical-align:top}
-        </style>
+        </style>-->
         <table class="tg">
             <!--<tr>
                 <th>date</th>
@@ -26,7 +26,7 @@
             <tr>
                 <td><?php echo date("d-m-Y H:i",strtotime($posts[$i]->date_creation()));?></td>
                 <td><a href="index.php?action=post&postId=<?php echo $posts[$i]->id(); ?>"><?php echo $posts[$i]->title();?></a></td>
-                <td><?php echo substr($posts[$i]->content(),0,30)."...";?></td>
+                <td><?php echo strip_tags(substr($posts[$i]->content(),0,30))."...";?></td>
                 <td><a href="index.php?action=editPostForm&id=<?php echo $posts[$i]->id();?>"> <i class="fas fa-pencil-alt"></i></a></td>
                 <td><form method="post" action="">
                     <input type="hidden" name="post_delete" value="<?php echo $posts[$i]->id(); ?>">
