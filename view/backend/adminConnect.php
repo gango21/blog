6a4efa974fd96df65edca63c89b41fa141c5fb7c
Morgan Taglang <morgan.tag@gmail.com>
2019-02-11 23:51:58 +0100

    afficher message d'erreur

--- view/backend/adminConnect.php
+++ view/backend/adminConnect.php
@@ -7,10 +7,16 @@
     <div class="admin">
         <p>Entrez votre nom d'utilisateur et votre mot de passe</p>
         <form action="index.php?action=admin" method="post">
+            <?php $fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+            if (strpos($fullUrl, "id_error=yes") == true ) {
+                echo "<p>L'identifiant ou le mot de passe est faux<br>";
+            }
+            ?>
             <p>
                 <input type="text" name="user" value="user" class="form" /><br>
                 <input type="password" name="password" value="password" class="form" /><br>
-                <input type="submit" value="LOGIN" />
+                <input type="submit" value="LOGIN" name="login-submit"/>
             </p>
         </form>
     </div>
