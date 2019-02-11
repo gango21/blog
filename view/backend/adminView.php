26c3f92cff1762ead207a198b5e0c149d7006278
Morgan Taglang <morgan.tag@gmail.com>
2019-02-11 20:49:56 +0100

    affiche uniquement le menu d'admin

--- view/backend/adminView.php
+++ view/backend/adminView.php
@@ -3,22 +3,7 @@
 <?php $title="Admin"; ?>
 <?php ob_start();
 
- if (!isset($_SESSION['admin'])){ ?>
-<div class="admin_page">
-    <div class="admin">
-        <p>Entrez votre nom d'utilisateur et votre mot de passe</p>
-        <form action="index.php?action=admin" method="post">
-            <p>
-                <input type="text" name="user" value="user" class="form" /><br>
-                <input type="password" name="password" value="password" class="form" /><br>
-                <input type="submit" value="LOGIN" />
-            </p>
-        </form>
-    </div>
-</div>
-<?php
-    }
-    else
+ if (isset($_SESSION['admin'])){ ?>
     {
     ?>
 <div class="admin_page">
