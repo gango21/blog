397c826a6530c3f455f86c6b77c43d3dc00a8b66
Morgan Taglang <morgan.tag@gmail.com>
2019-03-01 20:59:03 +0100

    script de changement de pages + modif email

--- view/template.php
+++ view/template.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
     <script src="public/js/nav.js"></script>
+    <script src="public/js/navpage.js"></script>
+
     <meta content="width=device-width, initial-scale=1" name="viewport" />
 </head>
 <header>
@@ -31,6 +33,7 @@
                     <a href="index.php?action=editPost&page=1">Modifier un billet</a>
                     <a href="index.php?action=deleteComment">Modérer les commentaires</a>
                     <a href="index.php?action=editPassword">Modifier le mot de passe</a>
+                    <a href="index.php?action=editEmail">Modifier votre email</a>
                 </div>
                 <?php
     }
