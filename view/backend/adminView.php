04bd6a8f2bcc0486939139db29aad6a075455e01
Morgan Taglang <morgan.tag@gmail.com>
2019-03-01 20:55:24 +0100

    Lien vers modification de l'email

--- view/backend/adminView.php
+++ view/backend/adminView.php
@@ -4,8 +4,6 @@
 <?php ob_start();
 
  if (isset($_SESSION['admin'])){ ?>
-    {
-    ?>
 <div class="admin_page">
     <div class="admin">
         <p><a href="index.php?action=globalView">Vue d'ensemble</a></p>
@@ -13,6 +11,7 @@
         <p><a href="index.php?action=editPost&page=1">Modifier un billet</a></p>
         <p><a href="index.php?action=deleteComment">Modérer les commentaires</a></p>
         <p><a href="index.php?action=editPassword">Modifier le mot de passe</a></p>
+        <p><a href="index.php?action=editEmail">Modifier votre email</a></p>
     </div>
 </div>
 <?php
